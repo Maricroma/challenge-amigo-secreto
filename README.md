dacedfb624d1a415354620244e7f075572d0e56b
Maricroma <cris.apdg@gmail.com>
2025-08-02 00:12:09 -0300

    Se agregaron funciones y lógica de la app. Se agregó archivo README

---
+++ README.md
@@ -0,0 +1,32 @@
+# 🎁 Amigo Secreto
+
+Este es un proyecto desarrollado como parte de un desafío para el Programa ONE de Alura Latam y Oracle.
+
+## 🧩 Descripción del proyecto
+
+Esta aplicación permite a los usuarios ingresar nombres de amigos en una lista para luego realizar un sorteo aleatorio y determinar quién es el "amigo secreto".
+
+## 🚀 Funcionalidades principales
+
+- **Agregar nombres**: Los usuarios pueden escribir un nombre en un campo de texto y hacer clic en el botón "Adicionar" para agregarlo a la lista.
+
+![demo de la aplicación](preview/demo1.png)
+
+- **Validación de entrada**: Si el campo está vacío, se muestra una alerta indicando que debe ingresarse un nombre válido.
+
+- **Visualización de la lista**: Los nombres ingresados se muestran en pantalla, debajo del campo de entrada.
+
+![demo de la aplicación](preview/demo2.png)
+
+- **Sorteo aleatorio**: Al hacer clic en el botón "Sortear Amigo", se elige un nombre al azar de la lista y se muestra el resultado en pantalla.
+
+![demo de la aplicación](preview/demo3.png)
+
+## 🛠️ Tecnologías utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+### ¡Gracias por visitar el repositorio! 🎉
+
--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const amigo = input.value.trim();
+    if (amigo) {
+        amigos.push(amigo);
+        input.value = "";
+        input.focus();
+        actualizarAmigos();
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+}
+
+function actualizarAmigos() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; 
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos para sortear.");
+        return;
+    }
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+    const resultado = document.getElementById("resultado");
+    resultado.textContent = `El amigo secreto sorteado es: ${amigoSorteado}`;
+    amigos = [];
+    actualizarAmigos();
+}
